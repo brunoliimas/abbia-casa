26403d6d936eec8a3713d014c4008e0edd2eaa43
brunocriacoes <br.rafael@outlook.com>
2023-02-06 12:06:19 -0300

    fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,15 +19,20 @@
 ;(function(){
     let $navPop = document.querySelector("#js-nav-pop")
     let $navIco = document.querySelector("#js-nav-ico")
+    let closeMenu = null
     if($navPop && $navIco ) {
+        $navPop.addEventListener( 'mouseover', function() {      
+            $navPop.classList.add('left-0')
+            clearTimeout(closeMenu)
+        } ) 
         $navIco.addEventListener( 'mouseover', function() {      
             $navPop.classList.add('left-0')
+            clearTimeout(closeMenu)
         } )
         $navPop.addEventListener('mouseout', function() {
-            $navPop.classList.remove('left-0')
-        })
-        $navPop.addEventListener('click', function() {
-            $navPop.classList.remove('left-0')
+            closeMenu = setTimeout(() => {
+                $navPop.classList.remove('left-0')
+            }, 100)
         })
     }
 })()
