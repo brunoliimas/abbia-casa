0fb8a76760b571cc8907dc5bd11139573fd0016d
brunocriacoes <br.rafael@outlook.com>
2023-08-03 09:51:33 -0300

    single fix

--- single-product.php
+++ single-product.php
@@ -126,7 +126,7 @@ apiJson($prod);
             </h2>
 
             <div class="overflow-x-hidden overflow-x-scroll lg:overflow-x-hidden  w-[100vw] lg:w-full  gap-4">
-                <div class="grid grid-col-3 w-full gap-4">
+                <div class="grid grid-cols-3 w-full gap-4">
                     <?php foreach ($novidade as $index => $prod) { ?>
                         <div class=" break-inside-avoid-column text-cor1 relative">
                             <a href="<?php echo $prod["linkSingle"]; ?>" title="<?php echo $prod["title"]; ?>">
